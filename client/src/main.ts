3d7a897d4d6a3ee043c9150b787985e4ca264b6a
Andy <10andy123456789@gmail.com>
2026-04-13 21:25:05 +0800

    修改最終版

--- client/src/main.ts
+++ client/src/main.ts
@@ -72,7 +72,7 @@ async function startGame() {
   menuScreen.classList.add('hidden');
   
   try {
-    await game.connect(SERVER_URL);
+    await game.connect(SERVER_Url)
     game.joinGame(playerName, skin);
     
     // Hide loading, show HUD
